c3a1d95be20067c3033810d4462772491563a1c7
Subash CK <subashchandrakanumuri@gmail.com>
2019-03-05 12:59:50 +0530

    new

--- mrcnn/config.py
+++ mrcnn/config.py
@@ -89,7 +89,7 @@ class Config(object):
     RPN_NMS_THRESHOLD = 1.0
 
     # How many anchors per image to use for RPN training
-    RPN_TRAIN_ANCHORS_PER_IMAGE = 200
+    RPN_TRAIN_ANCHORS_PER_IMAGE = 512
     
     # ROIs kept after tf.nn.top_k and before non-maximum suppression
     PRE_NMS_LIMIT = 6000
@@ -144,7 +144,7 @@ class Config(object):
     # enough positive proposals to fill this and keep a positive:negative
     # ratio of 1:3. You can increase the number of proposals by adjusting
     # the RPN NMS threshold.
-    TRAIN_ROIS_PER_IMAGE = 200
+    TRAIN_ROIS_PER_IMAGE = 512
 
     # Percent of positive ROIs used to train classifier/mask heads
     ROI_POSITIVE_RATIO = 0.33
@@ -155,7 +155,7 @@ class Config(object):
 
     # Shape of output mask
     # To change this you also need to change the neural network mask branch
-    MASK_SHAPE = [112, 112]
+    MASK_SHAPE = [56, 56]
 
     # Maximum number of ground truth instances to use in one image
     MAX_GT_INSTANCES = 100
