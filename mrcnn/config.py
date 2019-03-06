4c8c3c7f4d42349778e343656e67f92c10a15452
Subash CK <subashchandrakanumuri@gmail.com>
2019-03-06 10:48:00 +0530

    Update config.py

--- mrcnn/config.py
+++ mrcnn/config.py
@@ -89,7 +89,7 @@ class Config(object):
     RPN_NMS_THRESHOLD = 1.0
 
     # How many anchors per image to use for RPN training
-    RPN_TRAIN_ANCHORS_PER_IMAGE = 448
+    RPN_TRAIN_ANCHORS_PER_IMAGE = 512
     # ROIs kept after tf.nn.top_k and before non-maximum suppression
     PRE_NMS_LIMIT = 6000
 
@@ -100,7 +100,7 @@ class Config(object):
     # If enabled, resizes instance masks to a smaller size to reduce
     # memory load. Recommended when using high-resolution images.
     USE_MINI_MASK = True
-    MINI_MASK_SHAPE = (448,448)  # (height, width) of the mini-mask
+    MINI_MASK_SHAPE = (512,512)  # (height, width) of the mini-mask
 
     # Input image resizing
     # Generally, use the "square" resizing mode for training and predicting
@@ -143,7 +143,7 @@ class Config(object):
     # enough positive proposals to fill this and keep a positive:negative
     # ratio of 1:3. You can increase the number of proposals by adjusting
     # the RPN NMS threshold.
-    TRAIN_ROIS_PER_IMAGE = 448
+    TRAIN_ROIS_PER_IMAGE = 512
 
     # Percent of positive ROIs used to train classifier/mask heads
     ROI_POSITIVE_RATIO = 0.33
