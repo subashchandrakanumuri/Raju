9f69ccb758872df02865cd8d66181e74565cde6e
Subash CK <subashchandrakanumuri@gmail.com>
2019-03-07 12:02:09 +0530

    Update config.py

--- mrcnn/config.py
+++ mrcnn/config.py
@@ -100,7 +100,7 @@ class Config(object):
     # If enabled, resizes instance masks to a smaller size to reduce
     # memory load. Recommended when using high-resolution images.
     USE_MINI_MASK = True
-    MINI_MASK_SHAPE = (392,392)  # (height, width) of the mini-mask
+    MINI_MASK_SHAPE = (336,336)  # (height, width) of the mini-mask
 
     # Input image resizing
     # Generally, use the "square" resizing mode for training and predicting
