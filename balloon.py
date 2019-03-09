7d93643f8e3f94bfa4a50ee82f1e0ac8db19254e
Subash CK <subashchandrakanumuri@gmail.com>
2019-03-09 10:29:28 +0530

    sol

--- balloon.py
+++ balloon.py
@@ -281,7 +281,7 @@ def train(model):
     model.train(dataset_train, dataset_val,
                 learning_rate=config.LEARNING_RATE,
                 epochs=100,
-                layers='all')
+                layers='heads')
 
 
 def color_splash(image, mask):
